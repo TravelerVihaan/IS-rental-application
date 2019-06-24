a04b303b88f7ac425aa0aa40db63342c058ba747
Łukasz Biel <lbiel@grupa.onet>
2019-06-24 10:49:38 +0200

    Initial commit

---
+++ src/main/java/onet/grupa/isrentalapplication/IsRentalApplication.java
@@ -0,0 +1,13 @@
+package onet.grupa.isrentalapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsRentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/onet/grupa/isrentalapplication/IsRentalApplicationTests.java
@@ -0,0 +1,16 @@
+package onet.grupa.isrentalapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IsRentalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
