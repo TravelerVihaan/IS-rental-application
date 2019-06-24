2a645007e0bbac62754a432fd56828f621d06753
TravelerVihaan <lukasz.biel@10g.pl>
2019-06-24 12:04:30 +0200

    Create first classes

--- src/main/java/onet/grupa/isrentalapplication/domain/Rental.java
+++ src/main/java/onet/grupa/isrentalapplication/domain/Rental.java
@@ -1,4 +1,19 @@
 package onet.grupa.isrentalapplication.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "rentals")
 public class Rental {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_rental")
+    private Long id;
+
+    public Rental() {}
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
 }
--- src/main/java/onet/grupa/isrentalapplication/repository/RentalRepository.java
+++ src/main/java/onet/grupa/isrentalapplication/repository/RentalRepository.java
@@ -1,4 +1,10 @@
 package onet.grupa.isrentalapplication.repository;
 
-public class RentalRepository {
+import onet.grupa.isrentalapplication.domain.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RentalRepository extends JpaRepository<Rental, Long> {
+
 }
