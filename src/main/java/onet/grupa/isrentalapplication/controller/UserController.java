fe37e5cb7ee724773deae0e240bb49da1faa133d
TravelerVihaan <lukasz.biel@10g.pl>
2019-06-24 12:36:33 +0200

    Create REST controllers

--- src/main/java/onet/grupa/isrentalapplication/controller/UserController.java
+++ src/main/java/onet/grupa/isrentalapplication/controller/UserController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rentals")
-public class RentalController {
+@RequestMapping("/users")
+public class UserController {
 }
--- src/main/java/onet/grupa/isrentalapplication/repository/UserRepository.java
+++ src/main/java/onet/grupa/isrentalapplication/repository/UserRepository.java
@@ -2,6 +2,8 @@ package onet.grupa.isrentalapplication.repository;
 
 import onet.grupa.isrentalapplication.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
