21f0780c3eedcf7b6018ec4a3dba054b50466019
TravelerVihaan <lukasz.biel@10g.pl>
2019-06-26 14:12:58 +0200

    Computer service and controller

--- src/main/java/onet/grupa/isrentalapplication/controller/computers/ComputerController.java
+++ src/main/java/onet/grupa/isrentalapplication/controller/computers/ComputerController.java
@@ -25,21 +25,31 @@ public class ComputerController {
         this.computerService = computerService;
     }
 
+    /*
+    Get all computers from database
+     */
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<List<Computer>> getComputers(){
-        return null;
+    public ResponseEntity<List<Computer>> getComputers(){
+        return new ResponseEntity<>(computerService.getAllComputers(), new HttpHeaders(), HttpStatus.OK);
     }
 
+    /*
+    Get specified computer from database by id
+    @param id - id of computer from database
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Computer> getComputer(@PathVariable long id){
         return new ResponseEntity<>(computerService.findComputerById(id), new HttpHeaders(), HttpStatus.OK);
     }
 
+    /*
+    Update existing computer from database
+     */
     @PutMapping("/{id}")
     public ResponseEntity<Void> addComputer(@PathVariable("id") Long id, @RequestBody @Valid Computer computer, BindingResult result){
         if (result.hasErrors())
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        return new ResponseEntity(computerService.updateComputer(id,computer), new HttpHeaders(), HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new HttpHeaders(), HttpStatus.OK);
 
 
     }
