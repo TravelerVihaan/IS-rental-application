04d0f1734daf1b6333e1c6adda219d0b8633f43c
TravelerVihaan <lukasz.biel@10g.pl>
2019-06-27 11:10:49 +0200

    Added computerProducer Controller

--- src/main/java/onet/grupa/isrentalapplication/controller/computers/ComputerProducerController.java
+++ src/main/java/onet/grupa/isrentalapplication/controller/computers/ComputerProducerController.java
@@ -1,9 +1,39 @@
 package onet.grupa.isrentalapplication.controller.computers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import onet.grupa.isrentalapplication.domain.computers.ComputerProducer;
+import onet.grupa.isrentalapplication.service.computers.ComputerProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/computers/producers")
 public class ComputerProducerController {
+
+    private ComputerProducerService computerProducerService;
+
+    @Autowired
+    public ComputerProducerController(ComputerProducerService computerProducerService){
+        this.computerProducerService = computerProducerService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ComputerProducer>> getAllComputerProducers(){
+        return computerProducerService.getResponseWithAllComputerProducers();
+    }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ComputerProducer> getDisk(@PathVariable long id){
+        return computerProducerService.getResponseWithComputerProducer(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> addDiskType(@RequestBody @Valid ComputerProducer computerProducer, BindingResult result){
+        return computerProducerService.addNewDiskType(computerProducer, result);
+    }
 }
---
+++ src/main/java/onet/grupa/isrentalapplication/service/computers/ComputerProducerService.java
@@ -0,0 +1,60 @@
+package onet.grupa.isrentalapplication.service.computers;
+
+import onet.grupa.isrentalapplication.domain.computers.ComputerProducer;
+import onet.grupa.isrentalapplication.repository.computers.ComputerProducerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ComputerProducerService {
+
+    private ComputerProducerRepository computerProducerRepository;
+
+    @Autowired
+    public ComputerProducerService(ComputerProducerRepository computerProducerRepository){
+        this.computerProducerRepository = computerProducerRepository;
+    }
+
+    /*
+    Public methods
+     */
+    public ResponseEntity<List<ComputerProducer>> getResponseWithAllComputerProducers(){
+        return ResponseEntity.of(getAllComputerProducers());
+    }
+
+    public ResponseEntity<ComputerProducer> getResponseWithComputerProducer(long id){
+        return ResponseEntity.of(getComputerProducer(id));
+    }
+
+    public ResponseEntity<?> addNewDiskType(ComputerProducer computerProducer, BindingResult result){
+        if(result.hasErrors())
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if(getAllComputerProducers().isPresent()){
+            if(getAllComputerProducers()
+                    .get()
+                    .stream()
+                    .anyMatch(disk -> disk.getProducerName()
+                            .equalsIgnoreCase(computerProducer.getProducerName())))
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        computerProducerRepository.save(computerProducer);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    /*
+    Private methods
+     */
+    private Optional<ComputerProducer> getComputerProducer(long id){
+        return computerProducerRepository.findById(id);
+    }
+
+    private Optional<List<ComputerProducer>> getAllComputerProducers(){
+        return Optional.ofNullable(computerProducerRepository.findAll());
+    }
+}
