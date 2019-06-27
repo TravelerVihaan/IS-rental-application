e5374229db5ceb7504d84a7c8ea3d60e8e90791f
TravelerVihaan <lukasz.biel@10g.pl>
2019-06-27 11:57:31 +0200

    ComputerModelService and Controller

--- src/main/java/onet/grupa/isrentalapplication/controller/computers/ComputerModelController.java
+++ src/main/java/onet/grupa/isrentalapplication/controller/computers/ComputerModelController.java
@@ -1,10 +1,39 @@
 package onet.grupa.isrentalapplication.controller.computers;
 
+import onet.grupa.isrentalapplication.domain.computers.ComputerModel;
+import onet.grupa.isrentalapplication.service.computers.ComputerModelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("computers/models")
 public class ComputerModelController {
 
+    private ComputerModelService computerModelService;
 
+    @Autowired
+    public ComputerModelController(ComputerModelService computerModelService){
+        this.computerModelService = computerModelService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ComputerModel>> getAllComputerModels(){
+        return computerModelService.getResponseWithAllComputerModels();
+    }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ComputerModel> getComputerModel(@PathVariable long id){
+        return computerModelService.getResponseWithComputerModel(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> addComputerProducer(@RequestBody @Valid ComputerModel computerModel, BindingResult result){
+        return computerModelService.addNewComputerModel(computerModel, result);
+    }
 }
--- src/main/java/onet/grupa/isrentalapplication/service/computers/ComputerModelService.java
+++ src/main/java/onet/grupa/isrentalapplication/service/computers/ComputerModelService.java
@@ -1,8 +1,15 @@
 package onet.grupa.isrentalapplication.service.computers;
 
+import onet.grupa.isrentalapplication.domain.computers.ComputerModel;
 import onet.grupa.isrentalapplication.repository.computers.ComputerModelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ComputerModelService {
@@ -13,4 +20,48 @@ public class ComputerModelService {
     public ComputerModelService(ComputerModelRepository computerModelRepository){
         this.computerModelRepository = computerModelRepository;
     }
+
+    /*
+   Public methods
+    */
+    public ResponseEntity<List<ComputerModel>> getResponseWithAllComputerModels(){
+        return ResponseEntity.of(getAllComputerModels());
+    }
+
+    public ResponseEntity<ComputerModel> getResponseWithComputerModel(long id){
+        return ResponseEntity.of(getComputerModel(id));
+    }
+
+    /*
+     * This function add new Disk Type to database
+     * @param diskType
+     * @param binding result
+     */
+    public ResponseEntity<?> addNewComputerModel(ComputerModel computerModel, BindingResult result){
+        if(result.hasErrors())
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if(getAllComputerModels().isPresent()){
+            if(getAllComputerModels()
+                    .get()
+                    .stream()
+                    .filter(cmodel -> cmodel.getModel().equalsIgnoreCase(computerModel.getModel()))
+                    .filter(cmodel -> cmodel.getComputerProducer().getProducerName().equalsIgnoreCase(computerModel.getComputerProducer().getProducerName()))
+                    .count() > 0)
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        computerModelRepository.save(computerModel);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    /*
+    Private methods
+     */
+    private Optional<ComputerModel> getComputerModel(long id){
+        return computerModelRepository.findById(id);
+    }
+
+    private Optional<List<ComputerModel>> getAllComputerModels(){
+        return Optional.ofNullable(computerModelRepository.findAll());
+    }
+
 }
