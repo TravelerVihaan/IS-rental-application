3ee46f9cf68d4b38b98409a421004e0b165c24b8
TravelerVihaan <lukasz.biel@10g.pl>
2019-07-03 14:34:49 +0200

    Test

--- src/test/java/onet/grupa/isrentalapplication/IsRentalApplicationTests.java
+++ src/test/java/onet/grupa/isrentalapplication/IsRentalApplicationTests.java
@@ -2,21 +2,41 @@ package onet.grupa.isrentalapplication;
 
 import onet.grupa.isrentalapplication.service.computers.ComputerService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.Computer;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Optional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class IsRentalApplicationTests {
 
+
+    @Autowired
+    WebApplicationContext webCtx;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webCtx).build();
+    }
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws Exception {
+        mockMvc.perform(get("/computers/disks/")).andExpect(status().isOk());
     }
 
 }
