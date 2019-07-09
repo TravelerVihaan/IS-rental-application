f7e4ffd2dfd8bedb43f89bfd796dd9dba035b8dc
TravelerVihaan <lukasz.biel@10g.pl>
2019-07-09 13:55:43 +0200

    ReminderMails scheduled task and service

--- src/main/java/onet/grupa/isrentalapplication/service/mails/ReminderMailService.java
+++ src/main/java/onet/grupa/isrentalapplication/service/mails/ReminderMailService.java
@@ -3,9 +3,11 @@ package onet.grupa.isrentalapplication.service.mails;
 import onet.grupa.isrentalapplication.domain.rentals.ComputerRental;
 import onet.grupa.isrentalapplication.service.rentals.ComputerRentalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -15,6 +17,9 @@ public class ReminderMailService {
     private ComputerRentalService computerRentalService;
 
     private final String MAIL_FROM = "";
+    private final String MAIL_SUBJECT = "[WYPOZYCZALNIA] PROSIMY O ZWROT POZYCZONEGO SPRZETU";
+    private final String MAIL_TEXT = "Upłynął termin, który zadeklarowałeś jako datę zwrotu sprzętu. \n" +
+                                    " Dział Internal IT prosi o niezwłoczny zwrot wyporzyczonego sprzętu.";
 
     private List<String> mailsList;
 
@@ -26,13 +31,34 @@ public class ReminderMailService {
 
     public void sendMailsWithReminds(){
         getCurrentMailsList();
+        prepareAndSendReminderMails();
+    }
+
+    private void prepareAndSendReminderMails(){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(setRecipientList());
+        mailMessage.setFrom(MAIL_FROM);
+        mailMessage.setSubject(MAIL_SUBJECT);
+        mailMessage.setText(MAIL_TEXT);
+        mailSender.send(mailMessage);
+    }
+
+    private String[] setRecipientList(){
+        String[] recipientList = new String[mailsList.size()];
+        mailsList.toArray(recipientList);
+        return recipientList;
     }
 
     private void getCurrentMailsList(){
-        computerRentalService.getAllComputerRentalsWithStatus("active").ifPresent(this::addCurrentMailsToList);
+        computerRentalService.getAllComputerRentalsWithStatus("active")
+                .ifPresent(this::addCurrentMailsToList);
     }
 
     private void addCurrentMailsToList(List<ComputerRental> rentalsList){
-        rentalsList.stream().forEach( rental -> mailsList.add(rental.getRentingPersonemail()));
+        rentalsList
+                .stream()
+                .filter(rental -> rental.getEndRentalDate()
+                        .isBefore(LocalDate.now()))
+                .forEach(rental -> mailsList.add(rental.getRentingPersonemail()));
     }
 }
