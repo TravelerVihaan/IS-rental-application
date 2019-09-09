d0cca22972e2912a49ee2c0c5d1985ee5d519ef3
TravelerVihaan <lukasz.biel@10g.pl>
2019-09-09 16:18:02 +0200

    ComputerSearchingServiceTest

--- src/test/java/onet/grupa/isrentalapplication/service/computers/ComputerSearchingServiceTest.java
+++ src/test/java/onet/grupa/isrentalapplication/service/computers/ComputerSearchingServiceTest.java
@@ -1,11 +1,12 @@
 package onet.grupa.isrentalapplication.service.computers;
 
+import onet.grupa.isrentalapplication.domain.computers.*;
 import onet.grupa.isrentalapplication.repository.computers.ComputerRepository;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.Computer;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -27,11 +28,61 @@ public class ComputerSearchingServiceTest {
     @Before
     public void setUp(){
         computerSearchingService = new ComputerSearchingService(computerRepository,computerOrderingService);
+
     }
 
     @Test
     public void getSearchedComputersTest(){
+        Mockito.when(computerRepository.findAllByOtnumberContaining(any))
 
+    }
+
+    private onet.grupa.isrentalapplication.domain.computers.Computer getComputer1(){
+        ComputerProducer apple = new ComputerProducer("Apple");
+        ComputerModel macBook = new ComputerModel("MacBook",apple);
+        ComputerStatus computerStatus = new ComputerStatus("rented");
+        DiskType diskType = new DiskType("SSD");
+        OperatingSystem os = new OperatingSystem("Windows 10");
+        onet.grupa.isrentalapplication.domain.computers.Computer computer = new onet.grupa.isrentalapplication.domain.computers.Computer();
+        computer.setOtnumber("0000/56/7890/IT/KR");
+        computer.setSerialNumber("XYZABCD1312");
+        computer.setOperatingSystem(os);
+        computer.setDiskType(diskType);
+        computer.setComputerModel(macBook);
+        computer.setComputerStatus(computerStatus);
+        return computer;
+    }
+
+    private onet.grupa.isrentalapplication.domain.computers.Computer getComputer2(){
+        ComputerProducer apple = new ComputerProducer("DELL");
+        ComputerModel macBook = new ComputerModel("E6440",apple);
+        ComputerStatus computerStatus = new ComputerStatus("rented");
+        DiskType diskType = new DiskType("SSD");
+        OperatingSystem os = new OperatingSystem("Windows 10");
+        onet.grupa.isrentalapplication.domain.computers.Computer computer = new onet.grupa.isrentalapplication.domain.computers.Computer();
+        computer.setOtnumber("1111/56/7890/IT/KR");
+        computer.setSerialNumber("XYZABCD1312");
+        computer.setOperatingSystem(os);
+        computer.setDiskType(diskType);
+        computer.setComputerModel(macBook);
+        computer.setComputerStatus(computerStatus);
+        return computer;
+    }
+
+    private onet.grupa.isrentalapplication.domain.computers.Computer getComputer3(){
+        ComputerProducer apple = new ComputerProducer("Lenovo");
+        ComputerModel macBook = new ComputerModel("E7440",apple);
+        ComputerStatus computerStatus = new ComputerStatus("rented");
+        DiskType diskType = new DiskType("SSD");
+        OperatingSystem os = new OperatingSystem("Windows 10");
+        onet.grupa.isrentalapplication.domain.computers.Computer computer = new Computer();
+        computer.setOtnumber("2222/56/7890/IT/KR");
+        computer.setSerialNumber("XYZABCD1312");
+        computer.setOperatingSystem(os);
+        computer.setDiskType(diskType);
+        computer.setComputerModel(macBook);
+        computer.setComputerStatus(computerStatus);
+        return computer;
     }
 
 }
