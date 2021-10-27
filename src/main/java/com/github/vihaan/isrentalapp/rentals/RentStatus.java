110678721c4e9cc0f9c4f68e0cd745c227ddc864
TravelerVihaan <lukasz.biel@10g.pl>
2021-10-27 02:16:24 +0200

    RentStatus

--- src/main/java/com/github/vihaan/isrentalapp/rentals/RentStatus.java
+++ src/main/java/com/github/vihaan/isrentalapp/rentals/RentStatus.java
@@ -1,5 +1,20 @@
 package com.github.vihaan.isrentalapp.rentals;
 
 public enum RentStatus {
-    COMPLETED, WAITING, ACTIVE, OVERDUE, REJECTED;
+    COMPLETED("completed"),
+    WAITING("waiting"),
+    ACTIVE("active"),
+    OVERDUE("overdue"),
+    REJECTED("rejected"),
+    CANCELED("canceled");
+
+    private String status;
+
+    RentStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
---
+++ src/main/java/com/github/vihaan/isrentalapp/rentals/notifications/MailNotifier.java
@@ -0,0 +1,8 @@
+package com.github.vihaan.isrentalapp.rentals.notifications;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailNotifier {
+
+}
